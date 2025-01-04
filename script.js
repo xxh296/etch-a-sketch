7718dfdf05bf5e934f61ab55673ec6a08222d31f
xxh296 <140650758+xxh296@users.noreply.github.com>
2025-01-03 19:51:56 -0800

    Add the grid, and the drawing functionality * The grid resizes, depending on the assigned column number (done currently by changing the variable in the script, need to add this to UI). * There is still a small inconsistency with how the grid resizes.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("test");
+const container = document.querySelector("#container");
+// TODO : inconsistent sizing
+container.style.width = "980px";
+// TODO - get from user later
+let columnsNumber = 64;
+
+//placing divs in a grid
+for (let i = 0; i < columnsNumber*columnsNumber; i++) {
+    const div = document.createElement("div");  
+    
+    div.style.border = "1px solid blue"; 
+    
+    div.style.width = 960 / columnsNumber + "px";
+    div.style.height = 960 / columnsNumber + "px";
+    div.style.aspectRatio = 1/1;
+    div.style.display = "flex";
+    div.style.flexGrow = "1";
+
+    div.addEventListener("mouseover", () => div.style.backgroundColor = "white");
+    
+    container.appendChild(div);    
+    div.id = "div" + i;
+}
